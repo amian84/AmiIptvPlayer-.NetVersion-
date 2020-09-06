e9ff980ea21f95bb043f9405342d7b4825cd6ec9
damian <damian@gruposolutia.com>
2020-09-06 19:25:54 +0200

    Add entity to parse and store the channels info

---
+++ AmiIptvPlayer/ChannelInfo.cs
@@ -0,0 +1,49 @@
+﻿using PlaylistsNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AmiIptvPlayer
+{
+    public class ChannelInfo
+    {
+        public string Title { get; set; }
+        public string TVGName { get; set; }
+        public string TVGId { get; set; }
+        public string TVGGroup { get; set; }
+        public string TVGLogo { get; set; }
+        public string URL { get; set; }
+
+        public ChannelInfo(M3uPlaylistEntry channelEntry)
+        {
+            URL = channelEntry.Path;
+            string extraInfoForParser = "";
+            foreach (KeyValuePair<string, string> entry in channelEntry.CustomProperties)
+            {
+                extraInfoForParser += entry.Key + entry.Value;
+            }
+            string regexTVGName = "tvg-name=\"(.*?)\"";
+            string regexTVGLogo = "tvg-logo=\"(.*?)\"";
+            string regexTVGGroup = "group-title=\"(.*?)\"";
+            string regexTVGId = "tvg-id=\"(.*?)\"";
+            string regexTitle = "[,](?!.*[,])(.*?)$";
+            TVGName = MatchAndResult(extraInfoForParser, regexTVGName);
+            Title = MatchAndResult(extraInfoForParser, regexTitle);
+            TVGLogo = MatchAndResult(extraInfoForParser, regexTVGLogo);
+            TVGGroup = MatchAndResult(extraInfoForParser, regexTVGGroup);
+            TVGId = MatchAndResult(extraInfoForParser, regexTVGId);
+        }
+        private string MatchAndResult (string toSearch, string pattern)
+        {
+            Match result = Regex.Match(toSearch, pattern);
+            if (result.Success)
+            {
+                return result.Groups[1].Value;
+            }
+            return "";
+        }
+    }
+}
---
+++ AmiIptvPlayer/Channels.cs
@@ -0,0 +1,57 @@
+﻿
+using PlaylistsNET.Content;
+using PlaylistsNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmiIptvPlayer
+{
+    public class Channels
+    {
+        private static Channels instance;
+        private bool repaintList = false;
+        private Dictionary<int, ChannelInfo> channelsInfo = new Dictionary<int, ChannelInfo>();
+        public static Channels Get()
+        {
+            if (instance == null)
+            {
+                instance = new Channels();
+            }
+            return instance;
+        }
+        public void RefreshList(string _url)
+        {
+            string contents;
+            using (var wc = new WebClient())
+            {
+                contents = wc.DownloadString(_url);
+            }
+            
+            var parser = PlaylistParserFactory.GetPlaylistParser(".m3u");
+            IBasePlaylist playlist = parser.GetFromString(contents);
+            M3uPlaylist m3uList = (M3uPlaylist)playlist;
+            int channelNumber = 0;
+            foreach(M3uPlaylistEntry entry in m3uList.PlaylistEntries)
+            {
+                ChannelInfo channelInfo = new ChannelInfo(entry);
+                channelsInfo.Add(channelNumber, channelInfo);
+                channelNumber++;
+
+            }
+            this.repaintList = true;
+        }
+        public void SetRepaint(bool value)
+        {
+            this.repaintList = value;
+        }
+
+        public bool RePaint()
+        {
+            return this.repaintList;
+        }
+    }
+}
