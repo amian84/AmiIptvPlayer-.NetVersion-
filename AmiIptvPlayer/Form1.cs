710fcfcb39f52251ca13c586752d580d0a7b2095
damian <damian@gruposolutia.com>
2020-09-09 15:20:30 +0200

    Set default dummy channel if not list loaded
    Load list, paint list of channels and show loadingPanel
    Play if double click on channel

--- AmiIptvPlayer/Form1.cs
+++ AmiIptvPlayer/Form1.cs
@@ -26,22 +26,29 @@ namespace AmiIptvPlayer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            ListViewItem i = new ListViewItem("1");
-            i.SubItems.Add("La 1HD");
+            ListViewItem i = new ListViewItem("0");
+            i.SubItems.Add("Please load iptv list");
             chList.Items.Add(i);
             chList.FullRowSelect = true;
             axWindowsMediaPlayer1.URL = "http://test";
-            //axWindowsMediaPlayer1.Ctlcontrols.play();
-
-            //chList.Items.Add(new ListViewItem(new string[] { "2", "La 2HD" }));
         }
 
         private void listView1_DoubleClick(object sender, EventArgs e)
         {
+            Channels channels = Channels.Get();
             if (chList.SelectedItems.Count > 0)
             {
                 ListViewItem item = chList.SelectedItems[0];
-                MessageBox.Show(item.SubItems[1].ToString());
+                ChannelInfo channel = channels.GetChannel(int.Parse(item.SubItems[0].Text));
+                if (channel ==  null)
+                {
+                    MessageBox.Show(item.SubItems[1].Text);
+                }
+                else
+                {
+                    axWindowsMediaPlayer1.URL = channel.URL;
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                }
             }
         }
 
@@ -50,10 +57,41 @@ namespace AmiIptvPlayer
             Preferences pref = new Preferences();
             pref.ShowDialog();
             Channels channels = Channels.Get();
-            if (channels.RePaint())
+            if (channels.NeedRefresh())
             {
-                Console.Write("Repaint");
+                loadingPanel.Visible = true;
+                new System.Threading.Thread(delegate ()
+                {
+                    channels.RefreshList();
+                    fillChannelList();
+                    loadingPanel.Invoke((System.Threading.ThreadStart)delegate {
+                        loadingPanel.Visible = false;
+                    });
+                    
+                }).Start();
+                
             }
+        }
+        
+        private void fillChannelList()
+        {
+            Channels channels = Channels.Get();
+            
+            List<ListViewItem> listChannels = new List<ListViewItem>();
+            foreach (var elem in channels.GetChannelsDic())
+            {
+                int chNumber = elem.Key;
+                ChannelInfo channel = elem.Value;
+                ListViewItem i = new ListViewItem(chNumber.ToString());
+                i.SubItems.Add(channel.Title);
+                listChannels.Add(i);
+            }
+            chList.Invoke((System.Threading.ThreadStart)delegate {
+                chList.Items.Clear();
+                chList.Items.AddRange(listChannels.ToArray());
+            });
+            
+            
         }
 
         private void quitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -68,5 +106,9 @@ namespace AmiIptvPlayer
 
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
