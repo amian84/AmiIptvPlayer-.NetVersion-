11fdf18385ba3beb7e44fb7b44c0f78e7d568091
damian <damian@gruposolutia.com>
2020-09-14 17:37:15 +0200

    Initial class for EPG_DB

---
+++ AmiIptvPlayer/EPG_DB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmiIptvPlayer
+{
+    public class EPG_DB
+    {
+        private static EPG_DB _instance;
+        public bool Refresh { get; set; }
+        public static EPG_DB Get()
+        {
+            if (_instance == null)
+            {
+                _instance = new EPG_DB();
+            }
+            return _instance;
+        }
+    }
+}
