a10ab1e0680350ff5a86472a92b173497d012f43
damian <damian@gruposolutia.com>
2020-09-14 17:36:55 +0200

    Check EPG change and instance EPG_DB and set need refresh

--- AmiIptvPlayer/Preferences.cs
+++ AmiIptvPlayer/Preferences.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
@@ -49,10 +42,18 @@ namespace AmiIptvPlayer
                 Channels channels = Channels.Get();
                 channels.SetUrl(txtURL.Text);
                 channels.SetNeedRefresh(true);
-                config.Save(ConfigurationSaveMode.Modified);
                 ConfigurationManager.RefreshSection("appSettings");
             }
-            
+
+            string lastEPG = config.AppSettings.Settings["Epg"].Value;
+            if (lastEPG != txtEPG.Text)
+            {
+                config.AppSettings.Settings["Epg"].Value = txtEPG.Text;
+                EPG_DB epgDB = EPG_DB.Get();
+                epgDB.Refresh = true;
+                ConfigurationManager.RefreshSection("appSettings");
+            }
+            config.Save(ConfigurationSaveMode.Modified);
             this.Close();
             this.Dispose();
         }
