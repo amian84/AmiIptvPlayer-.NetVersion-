05e899a1718eb5033ec24e2b06a6e1ded31c7750
damian <damian@gruposolutia.com>
2020-09-29 12:35:45 +0200

    Added utils funcitions

--- AmiIptvPlayer/utils.cs
+++ AmiIptvPlayer/utils.cs
@@ -10,7 +10,17 @@ namespace AmiIptvPlayer
 {
     public class Utils
     {
-
+        public static Dictionary<string, string> audios = new Dictionary<string, string>
+        {
+            { "spa", "Spanish"},
+            { "eng", "English" }
+        };
+        public static Dictionary<string, string> subs = new Dictionary<string, string>
+        {
+            { "none", "None"},
+            { "spa", "Spanish"},
+            { "eng", "English" }
+        };
         public static async Task<string> GetAsync(string uri)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
@@ -22,6 +32,30 @@ namespace AmiIptvPlayer
             {
                 return await reader.ReadToEndAsync();
             }
+        }
+
+        public static string GetAudioConfName(string audioCombo)
+        {
+            foreach (KeyValuePair<string, string> entry in audios)
+            {
+                if (entry.Value == audioCombo)
+                {
+                    return entry.Key;
+                }
+            }
+            return "spa";
+        }
+
+        public static string GetSubConfName(string subCombo)
+        {
+            foreach (KeyValuePair<string, string> entry in subs)
+            {
+                if (entry.Value == subCombo)
+                {
+                    return entry.Key;
+                }
+            }
+            return "none";
         }
     }
 }
