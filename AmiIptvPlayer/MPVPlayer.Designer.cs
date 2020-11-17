f811292ff39f80e6e8cb17ab8a9ada5b4f87c476
David Amian Valle <amian84@gmail.com>
2020-11-17 17:00:30 +0100

    new windows for player disembedded

---
+++ AmiIptvPlayer/MPVPlayer.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace AmiIptvPlayer
+{
+    partial class MPVPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panelvideo = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panelvideo, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(535, 403);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panelvideo
+            // 
+            this.panelvideo.AutoSize = true;
+            this.panelvideo.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelvideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelvideo.ForeColor = System.Drawing.Color.Black;
+            this.panelvideo.Location = new System.Drawing.Point(2, 2);
+            this.panelvideo.Margin = new System.Windows.Forms.Padding(2);
+            this.panelvideo.Name = "panelvideo";
+            this.panelvideo.Size = new System.Drawing.Size(531, 318);
+            this.panelvideo.TabIndex = 24;
+            // 
+            // MPVPlayer
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(535, 403);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(551, 442);
+            this.Name = "MPVPlayer";
+            this.Text = "MPVPlayer";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panelvideo;
+    }
+}
