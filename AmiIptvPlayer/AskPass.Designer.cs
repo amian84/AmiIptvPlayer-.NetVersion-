5537e0792e8e3d9feed68d3eaea79c3dfce738c4
David Amian Valle <amian84@gmail.com>
2021-03-04 17:57:00 +0100

    New form to ask the parental control pwd

---
+++ AmiIptvPlayer/AskPass.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace AmiIptvPlayer
+{
+    partial class AskPass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AskPass));
+            this.lbAsk = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbAsk
+            // 
+            this.lbAsk.AutoSize = true;
+            this.lbAsk.Location = new System.Drawing.Point(19, 23);
+            this.lbAsk.Name = "lbAsk";
+            this.lbAsk.Size = new System.Drawing.Size(0, 13);
+            this.lbAsk.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(17, 55);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(260, 20);
+            this.txtPass.TabIndex = 1;
+            this.txtPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPass_KeyPress);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(121, 94);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(202, 94);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // AskPass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 146);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.lbAsk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AskPass";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "AskPass";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.AskPass_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbAsk;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
---
+++ AmiIptvPlayer/AskPass.cs
@@ -0,0 +1,56 @@
+﻿using AmiIptvPlayer.i18n;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmiIptvPlayer
+{
+    public partial class AskPass : Form
+    {
+        public AskPass()
+        {
+            InitializeComponent();
+        }
+
+        private void AskPass_Load(object sender, EventArgs e)
+        {
+            this.Text = Strings.ASK_PASS;
+            lbAsk.Text = Strings.lbAskPass;
+            btnCancel.Text = Strings.CANCEL;
+            btnOK.Text = "Ok";
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (Utils.Base64Encode(txtPass.Text) == AmiConfiguration.Get().PARENTAL_PASS)
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                lbAsk.Text = Strings.lbAskPassError + "\n" + Strings.lbAskPass;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void txtPass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                btnOK_Click(null, null);
+            }
+        }
+    }
+}
